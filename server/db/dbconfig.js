ca3a2f6b57a5fa8c8cb8699c9767a2f70d897e27
Stephen Peterson <stephen@anerrantrake.com>
2019-07-30 16:16:13 -0600

    #3 #4 - Adds login and registration to backend

---
+++ server/db/dbconfig.js
@@ -0,0 +1,25 @@
+//THIS FILE STAYS BASICALLY THE SAME
+import mongoose from 'mongoose'
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connection.on('error', err => {
+  console.error('[DB] Error:', err)
+})
+
+import keys from './dbkeys'
+const connectionString = `mongodb+srv://${keys.user}:${keys.password}@cluster0-4eckx.mongodb.net/bcw-capstone?retryWrites=true&w=majority`
+
+
+export default class DbContext {
+  static async connect() {
+    try {
+      let status = await mongoose.connect(connectionString)
+      console.log("[DB] CONNECTED")
+      return status
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
