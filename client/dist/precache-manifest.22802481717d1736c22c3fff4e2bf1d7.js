fd7c1130190a3268dd692330710f6ef87527eb9f
Stephen Peterson <stephen@anerrantrake.com>
2019-07-31 13:34:12 -0600

    #17 - deployment prep

---
+++ client/dist/precache-manifest.22802481717d1736c22c3fff4e2bf1d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a135a3baf6c72d71d42",
+    "url": "/css/app.82ea5ed1.css"
+  },
+  {
+    "revision": "5a135a3baf6c72d71d42",
+    "url": "/js/app.f1578da7.js"
+  },
+  {
+    "revision": "af20727ceee4bf9b66b1",
+    "url": "/js/ballotCreation.994e5d18.js"
+  },
+  {
+    "revision": "cd9b946a1500e37b7fdf",
+    "url": "/js/chunk-vendors.e4bcaa33.js"
+  },
+  {
+    "revision": "64d9ae5d529fe9ade1526d97cb6344ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
