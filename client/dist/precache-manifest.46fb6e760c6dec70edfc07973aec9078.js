c702a9b3e30753cac23f499bd0e3f51c47cd01e2
Stephen Peterson <stephen@anerrantrake.com>
2019-08-01 15:51:49 -0600

    Sprint 2 build

---
+++ client/dist/precache-manifest.46fb6e760c6dec70edfc07973aec9078.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeaebbb527413f62343e",
+    "url": "/css/app.cc7fbab0.css"
+  },
+  {
+    "revision": "eeaebbb527413f62343e",
+    "url": "/js/app.319909c0.js"
+  },
+  {
+    "revision": "0d2e653f1412c078abf0",
+    "url": "/js/ballot.3f3d6e39.js"
+  },
+  {
+    "revision": "57cd6a5a98a3dd8d51eb",
+    "url": "/js/ballotCreation.1ee332e1.js"
+  },
+  {
+    "revision": "cd9b946a1500e37b7fdf",
+    "url": "/js/chunk-vendors.e4bcaa33.js"
+  },
+  {
+    "revision": "70321ba0485d5ea1625e",
+    "url": "/js/election.c25c1bef.js"
+  },
+  {
+    "revision": "ef5b64c8b07ee5b3b709",
+    "url": "/js/electionStatus.d332a61c.js"
+  },
+  {
+    "revision": "3f38eaa7471d73b99a69e2109ea1fc81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
