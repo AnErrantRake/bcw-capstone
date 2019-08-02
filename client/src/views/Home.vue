80f4816567daf94e5095f4e15d3d238e38688082
Stephen Peterson <stephen@anerrantrake.com>
2019-08-02 14:56:19 -0600

    #43 #46 - Adds host set election timeout

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="home">
-    <div class="modal fade" id="dateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <div class="modal fade" id="dateModal" tabindex="-1" role="dialog" aria-labelledby="dateModalLabel"
       aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <h5 class="modal-title" id="dateModalLabel">{{activeBallot.name || ''}}</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">
-            ...
+          <div class="modal-body container">
+            <div class="row">
+              <div class="col-6">
+                <h3>Restaurants</h3>
+                <table>
+                  <tr v-for="nom in activeBallot.noms">
+                    <td>{{nom}}</td>
+                  </tr>
+                </table>
+              </div>
+              <div class="col-6">
+                <h3>End Time</h3>
+                <input type="time" v-model="timestamp" name="" id="">
+                <input type="date" v-model="date" name="" id="">
+              </div>
+            </div>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -28,8 +42,8 @@
     <ul>
       <li v-for="ballot in ballots">
         <router-link :to="{name: 'ballot', params:{ballotID: ballot._id}}">{{ballot.name}}</router-link>
-        <button @click="setCreationElectionID(ballot._id)" data-toggle="modal" data-target="#dateModal"
-          class="btn btn-primary" type="submit">Start</button>
+        <button @click="setActiveBallot(ballot)" data-toggle="modal" data-target="#dateModal" class="btn btn-primary"
+          type="submit">Start</button>
         <button @click="deleteBallot(ballot._id)" class="btn btn-danger" type="submit">Delete</button>
       </li>
     </ul>
@@ -45,6 +59,8 @@
 
 
 <script>
+  import moment from 'moment'
+
   export default {
     name: "home",
     mounted() {
@@ -54,10 +70,9 @@
     data() {
       return {
         showTimeDialog: false,
-        endTime: '',
         timestamp: '',
         date: '',
-        creationElectionID: ''
+        activeBallot: {}
       };
     },
     computed: {
@@ -75,11 +90,23 @@
       deleteElection(electionID) {
         this.$store.dispatch('deleteElection', electionID)
       },
-      setCreationElectionID(ballotID) {
-        this.creationElectionID = ballotID;
+      setActiveBallot(ballot) {
+        // set modal date values to 30 minutes in the future
+        let endTime = moment().add(30, 'minutes');
+        this.date = endTime.format('YYYY-MM-DD');
+        this.timestamp = endTime.format('HH:mm');
+
+        // set active ballot to input
+        this.activeBallot = ballot;
       },
       startElection() {
-        this.$store.dispatch('startElection', this.creationElectionID)
+        let timeout = moment(this.timestamp + ' ' + this.date, 'HH:mm YYYY-MM-DD');
+        let election = {
+          ballotID: this.activeBallot._id,
+          timeoutEpoch: timeout.valueOf()
+        }
+
+        this.$store.dispatch('startElection', election)
       },
       logout() {
         this.$store.dispatch('logout');
