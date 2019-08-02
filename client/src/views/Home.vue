99d7a63436651d926085bdc1b00a997f5c5b12d5
Stephen Peterson <stephen@anerrantrake.com>
2019-08-02 11:36:30 -0600

    #43 - Adds start election modal

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="home">
+    <div class="modal fade" id="dateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button @click="startElection" class="btn btn-primary" type="submit" data-dismiss="modal">Start
+              Election</button>
+          </div>
+        </div>
+      </div>
+    </div>
     <h1>Logged in!</h1>
     <button @click="logout" class="btn btn-warning" type="submit">Logout</button>
     <h1>Ballots!</h1>
@@ -7,7 +28,8 @@
     <ul>
       <li v-for="ballot in ballots">
         <router-link :to="{name: 'ballot', params:{ballotID: ballot._id}}">{{ballot.name}}</router-link>
-        <button @click='startElection(ballot._id)' class="btn btn-primary" type="submit">Start</button>
+        <button @click="setCreationElectionID(ballot._id)" data-toggle="modal" data-target="#dateModal"
+          class="btn btn-primary" type="submit">Start</button>
         <button @click="deleteBallot(ballot._id)" class="btn btn-danger" type="submit">Delete</button>
       </li>
     </ul>
@@ -30,7 +52,13 @@
       this.$store.dispatch('getElections');
     },
     data() {
-      return {};
+      return {
+        showTimeDialog: false,
+        endTime: '',
+        timestamp: '',
+        date: '',
+        creationElectionID: ''
+      };
     },
     computed: {
       ballots() {
@@ -47,8 +75,11 @@
       deleteElection(electionID) {
         this.$store.dispatch('deleteElection', electionID)
       },
-      startElection(ballotID) {
-        this.$store.dispatch('startElection', ballotID)
+      setCreationElectionID(ballotID) {
+        this.creationElectionID = ballotID;
+      },
+      startElection() {
+        this.$store.dispatch('startElection', this.creationElectionID)
       },
       logout() {
         this.$store.dispatch('logout');
