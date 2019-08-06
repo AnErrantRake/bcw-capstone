72d5098b858599aeee0e31b8fcc90d89b0dcb821
benffrost <benffrost@gmail.com>
2019-08-06 10:39:23 -0600

    login page much prettier

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="login">
+  <div class="login mt-4">
+    <h3>Joining a WaFL?</h3>
+    <form @submit.prevent="getElection">
+      <input type="text" v-model="pin" placeholder="pin" />
+      <button class="btn btn-success  btn-sm mb-1 mx-1" type="submit">What's for Lunch?</button>
+    </form>
+    <br>
+    <h3>Log In:</h3>
     <form v-if="loginForm" @submit.prevent="loginUser">
       <input type="text" v-model="creds.username" placeholder="Enter your name" />
       <input type="password" v-model="creds.password" placeholder="password" />
-      <button class="btn btn-success" type="submit">Login</button>
+      <button class="btn btn-success btn-sm mb-1 mx-1" type="submit">Login</button>
     </form>
     <form v-else @submit.prevent="register">
       <input type="text" v-model="newUser.username" placeholder="name" />
       <input type="password" v-model="newUser.password" placeholder="password" />
-      <button class="btn btn-warning" type="submit">Create Account</button>
+      <button class="btn btn-warning  btn-sm mb-1 mx-1" type="submit">Create Account</button>
     </form>
     <div class="action" @click="loginForm = !loginForm">
-      <p v-if="loginForm">No account? Click here to Register</p>
-      <p v-else>Already have an account? Click here to Login</p>
+      <p v-if="loginForm">No account? Click <span class="text-primary">here</span> to Register</p>
+      <p v-else>Already have an account? Click <span class="text-primary">here</span> to Login</p>
     </div>
-    <form @submit.prevent="getElection">
-      <input type="text" v-model="pin" placeholder="pin" />
-      <button class="btn btn-success" type="submit">What's for Lunch?</button>
-    </form>
   </div>
 </template>
 
