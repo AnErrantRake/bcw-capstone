234fe79d248280d09f7ed23c0840e01a16214f02
ChristopherOBerry <chroberry@gmail.com>
2019-08-06 09:40:14 -0600

    #62 page organization

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,51 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary" id="navbar">
+      <a class="navbar-brand text-center" href="/">
+        <i class="fas fa-hashtag"></i> WaFL - What's for Lunch?
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon text-white"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto text-right">
+          <li class="nav-item active">
+            <a class="nav-link" href="/#/ballot/new">New WaFL</a>
+          </li>
+          <li class="nav-item active">
+            <a href="#" class="nav-link">Start a WaFL</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link">Active WaFLs</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" style="cursor:pointer;" @click="logout">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
 <script>
 export default {
-  name: "App"
+  name: "App",
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+    }
+  }
 };
 </script>
 
+<style scoped>
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -21,35 +21,56 @@
               </div>
               <div class="col-6">
                 <h3>End Time</h3>
-                <input type="time" v-model="timestamp" name="" id="">
-                <input type="date" v-model="date" name="" id="">
+                <input type="time" v-model="timestamp" name id />
+                <input type="date" v-model="date" name id />
               </div>
             </div>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button @click="startElection" class="btn btn-primary" type="submit" data-dismiss="modal">Start
-              Election</button>
+            <button
+              @click="startElection"
+              class="btn btn-primary"
+              type="submit"
+              data-dismiss="modal"
+            >
+              Start
+              Election
+            </button>
           </div>
         </div>
       </div>
     </div>
-    <h1>Logged in!</h1>
-    <button @click="logout" class="btn btn-warning" type="submit">Logout</button>
-    <h1>Ballots!</h1>
-    <router-link class="btn btn-success" :to="{name: 'ballotCreation'}">Create New Ballot</router-link>
+    <div class="row text-center">
+      <div class="col-12 mt-3 mb-3">
+        <h1>Welcome to WFL!</h1>
+      </div>
+      <div class="col-12 mb-3">
+        <router-link class="btn btn-primary" :to="{name: 'ballotCreation'}">Create a new WFL</router-link>
+      </div>
+      <div class="col-12">
+        <h3>Start a WFL</h3>
+      </div>
+    </div>
     <ul>
       <li v-for="ballot in ballots">
         <router-link :to="{name: 'ballot', params:{ballotID: ballot._id}}">{{ballot.name}}</router-link>
-        <button @click="setActiveBallot(ballot)" data-toggle="modal" data-target="#dateModal" class="btn btn-primary"
-          type="submit">Start</button>
+        <button
+          @click="setActiveBallot(ballot)"
+          data-toggle="modal"
+          data-target="#dateModal"
+          class="btn btn-primary"
+          type="submit"
+        >Start</button>
         <button @click="deleteBallot(ballot._id)" class="btn btn-danger" type="submit">Delete</button>
       </li>
     </ul>
     <h1>Elections</h1>
     <ul>
       <li v-for="election in elections">
-        <router-link :to="{name: 'electionStatus', params:{electionID: election._id}}">{{election.pin}}</router-link>
+        <router-link
+          :to="{name: 'electionStatus', params:{electionID: election._id}}"
+        >{{election.pin}}</router-link>
         <button @click="deleteElection(election._id)" class="btn btn-danger" type="submit">Delete</button>
       </li>
     </ul>
@@ -58,61 +79,61 @@
 
 
 <script>
-  import moment from 'moment'
+import moment from "moment";
 
-  export default {
-    name: "home",
-    mounted() {
-      this.$store.dispatch('getBallots');
-      this.$store.dispatch('getElections');
+export default {
+  name: "home",
+  mounted() {
+    this.$store.dispatch("getBallots");
+    this.$store.dispatch("getElections");
+  },
+  data() {
+    return {
+      showTimeDialog: false,
+      timestamp: "",
+      date: "",
+      activeBallot: {}
+    };
+  },
+  computed: {
+    ballots() {
+      return this.$store.state.ballots;
     },
-    data() {
-      return {
-        showTimeDialog: false,
-        timestamp: '',
-        date: '',
-        activeBallot: {}
+    elections() {
+      return this.$store.state.elections;
+    }
+  },
+  methods: {
+    deleteBallot(ballotID) {
+      this.$store.dispatch("deleteBallot", ballotID);
+    },
+    deleteElection(electionID) {
+      this.$store.dispatch("deleteElection", electionID);
+    },
+    setActiveBallot(ballot) {
+      // set modal date values to 30 minutes in the future
+      let endTime = moment().add(30, "minutes");
+      this.date = endTime.format("YYYY-MM-DD");
+      this.timestamp = endTime.format("HH:mm");
+
+      // set active ballot to input
+      this.activeBallot = ballot;
+    },
+    startElection() {
+      let timeout = moment(
+        this.timestamp + " " + this.date,
+        "HH:mm YYYY-MM-DD"
+      );
+      let election = {
+        ballotID: this.activeBallot._id,
+        timeoutEpoch: timeout.valueOf()
       };
-    },
-    computed: {
-      ballots() {
-        return this.$store.state.ballots;
-      },
-      elections() {
-        return this.$store.state.elections;
-      }
-    },
-    methods: {
-      deleteBallot(ballotID) {
-        this.$store.dispatch('deleteBallot', ballotID);
-      },
-      deleteElection(electionID) {
-        this.$store.dispatch('deleteElection', electionID)
-      },
-      setActiveBallot(ballot) {
-        // set modal date values to 30 minutes in the future
-        let endTime = moment().add(30, 'minutes');
-        this.date = endTime.format('YYYY-MM-DD');
-        this.timestamp = endTime.format('HH:mm');
 
-        // set active ballot to input
-        this.activeBallot = ballot;
-      },
-      startElection() {
-        let timeout = moment(this.timestamp + ' ' + this.date, 'HH:mm YYYY-MM-DD');
-        let election = {
-          ballotID: this.activeBallot._id,
-          timeoutEpoch: timeout.valueOf()
-        }
-
-        this.$store.dispatch('startElection', election)
-      },
-      logout() {
-        this.$store.dispatch('logout');
-      }
-    },
-    components: {}
-  };
+      this.$store.dispatch("startElection", election);
+    }
+  },
+  components: {}
+};
 </script>
 
 
