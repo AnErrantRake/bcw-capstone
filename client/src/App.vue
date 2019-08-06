6e4cd1eea23678b3ad20d61644a1f41057e66706
benffrost <benffrost@gmail.com>
2019-08-06 10:31:08 -0600

    basic functionality

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,50 +1,52 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary" id="navbar">
-      <a class="navbar-brand text-center" href="/">
+      <router-link v-if="user._id" class="navbar-brand text-center" :to="{name: 'home'}">
         <i class="fas fa-hashtag"></i> WaFL - What's for Lunch?
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      </router-link>
+      <span v-else class="navbar-brand text-center">
+        <i class="fas fa-hashtag"></i> WaFL - What's for Lunch?
+      </span>
+
+      <button v-if="user._id" class="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon text-white"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div v-if="user._id" class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto text-right">
           <li class="nav-item active">
-            <a class="nav-link" href="/#/ballot/new">New WaFL</a>
+            <router-link :to="{name:'home'}" class="nav-link">Home</router-link>
           </li>
           <li class="nav-item active">
-            <a href="#" class="nav-link">Start a WaFL</a>
+            <router-link class="nav-link" :to="{name: 'ballotCreation'}">New WaFL</router-link>
           </li>
           <li class="nav-item active">
-            <a class="nav-link">Active WaFLs</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" style="cursor:pointer;" @click="logout">Logout</a>
+            <a class="nav-link" style="cursor:pointer;" @click="logout">Logout
+            </a>
           </li>
         </ul>
       </div>
+
     </nav>
     <router-view />
   </div>
 </template>
 <script>
-export default {
-  name: "App",
-  methods: {
-    logout() {
-      this.$store.dispatch("logout");
+  export default {
+    name: "App",
+    computed: {
+      user() {
+        return this.$store.state.user
+      }
+    },
+    methods: {
+      logout() {
+        this.$store.dispatch("logout");
+      }
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
