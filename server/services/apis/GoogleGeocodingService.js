4cdda625da1a3fe0756731da4008c51d31bf4332
Stephen Peterson <stephen@anerrantrake.com>
2019-08-06 12:37:58 -0600

    #77 - Reworks env vars to use WAFL prefix

--- server/services/apis/GoogleGeocodingService.js
+++ server/services/apis/GoogleGeocodingService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let _googleGeocodingApi = axios.create({
-  baseURL: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_API_KEY}`,
+  baseURL: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.WAFL_GOOGLE_API_KEY}`,
   timeout: 3000
 })
 
